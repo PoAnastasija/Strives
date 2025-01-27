2bfbb23d9d932ffbfff47914f972c3431743d059
PoAnastasija <anastasija.potapova@epitech.eu>
2025-01-27 23:41:28 +0100

    [+] added an initial commit to the project

---
+++ front-end/src/App.tsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <Router>
+      <Routes>
+          <Route path="/" element={<div>jhj</div>}/>
+      </Routes>
+    </Router>
+  )
+}
+
+export default App
